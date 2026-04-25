32ca0f1249f8cf6e9ec661ba152eeb2fdd8395c2
Michael D <dtmghislain@gmail.com>
2026-04-25 15:21:00 +0200

    feat(add style): testing new style

---
+++ assets/scripts/contact.js
@@ -0,0 +1,42 @@
+// --- Fonction ROT13 ---
+function rot13(str) {
+  return str.replace(/[a-zA-Z]/g, function(c){
+    return String.fromCharCode(
+      (c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13)
+      ? c : c - 26
+    );
+  });
+}
+
+// Email chiffré ROT13 (dtmghislain@gmail.com)
+const encryptedMail = "qgztuvfyvna@tznvy.pbz";
+const decodedMail = rot13(encryptedMail);
+
+document.getElementById("contactForm").addEventListener("submit", function(e){
+  e.preventDefault();
+
+  const nom = encodeURIComponent(document.getElementById("name").value);
+  const mailUser = encodeURIComponent(document.getElementById("email").value);
+
+  // Récupération des checkbox cochées
+  const checkboxes = document.querySelectorAll('input[name="choix"]:checked');
+  let selected = [];
+
+  checkboxes.forEach(cb => selected.push(cb.value));
+
+  // Génération automatique du message final
+  const messageGenere =
+      selected.length > 0
+      ? selected.join("%0D%0A- ")    
+      : "Aucun sujet sélectionné.";
+
+  const subject = encodeURIComponent("Contact depuis le site DevOps de Michael D.");
+
+  const body =
+      "Nom : " + nom + "%0D%0A" +
+      "Email : " + mailUser + "%0D%0A%0D%0A" +
+      "Sujet(s) choisi(s) :%0D%0A- " + messageGenere + "%0D%0A";
+
+  window.location.href =
+      "mailto:" + decodedMail + "?subject=" + subject + "&body=" + body;
+});
---
+++ assets/scripts/theme.js
@@ -0,0 +1,5 @@
+// Activate dark mode by override default.css with dark mode css
+function toggleTheme() {
+    const darkLink = document.getElementById('dark-theme');
+    darkLink.disabled = !darkLink.disabled;
+}
